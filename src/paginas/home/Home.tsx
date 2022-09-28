4f947f7a6bd3f1f122dbc63821731fe0372b03ef
renanlira96 <renanlira2@gmail.com>
2022-09-28 13:54:21 -0300

    Blog Pessoal React.

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -4,10 +4,10 @@ import './Home.css';
 function Home(){
     return (
         <>
-        <h1>Home</h1>
-        <img src="https://uploads.jovemnerd.com.br/wp-content/uploads/2021/09/imagem_2022_07_06_104044606__y10283k.png" alt="Imagem Tela Inicial" />
+        <h1 className="titulo">Home</h1>
+        <img src="https://uploads.jovemnerd.com.br/wp-content/uploads/2021/09/imagem_2022_07_06_104044606__y10283k.png" alt="Imagem Tela Inicial" className="img" />
         </>
     );
 }
 
-exports default Home; 
+export default Home; 
