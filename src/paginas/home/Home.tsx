ea69eed1a92bae2c4c062e14a699cb84acc8200c
renanlira96 <renanlira2@gmail.com>
2022-09-28 12:53:51 -0300

    Blog-pessoal React.

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+        <h1>Home</h1>
+        <img src="https://uploads.jovemnerd.com.br/wp-content/uploads/2021/09/imagem_2022_07_06_104044606__y10283k.png" alt="Imagem Tela Inicial" />
+        </>
+    );
+}
+
+exports default Home; 
