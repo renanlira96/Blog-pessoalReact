c7a579a296e3893a2a720da797fa737fd98a84ef
renanlira96 <renanlira2@gmail.com>
2022-09-30 14:06:56 -0300

    blogpessoal React criando login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
 
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Home />
-      <Footer />
-    </>
-  );
+  return(
+    <BrowserRouter>
+    <Navbar />
+    <div style={{ minHeight: '100vh' }}>
+    <Routes> // Antigo Switch
+    <Route path="/" element={<Login />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/home" element={<Home />} />
+    {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+    </Routes>
+    </div>
+    <Footer />
+    </ BrowserRouter >
+    );
+    
 }
 
 export default App;
