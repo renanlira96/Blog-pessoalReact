724ff0769c1f843bb51e9ceb0724c4f67985cb9d
renanlira96 <renanlira2@gmail.com>
2022-10-04 16:13:47 -0300

    Axios.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
 
 
@@ -16,7 +17,7 @@ function App() {
     <Route path="/" element={<Login />} />
     <Route path="/login" element={<Login />} />
     <Route path="/home" element={<Home />} />
-    {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+    <Route path="/cadastrousuario" element={<CadastroUsuario />} />
     </Routes>
     </div>
     <Footer />
